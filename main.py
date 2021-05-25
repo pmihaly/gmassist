24b9d03249f4c0658063741d5b4586d0ccb84f16
pmihaly <pappmihalyemil@protonmail.com>
2021-05-25 07:24:44 +0200

    removing joplin, new python version

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from blocks import date, greeting, weather
 import yaml
 from gtts import gTTS
 import datetime
+
 
 def write_mp3(text):
     with open(f"speeches/{datetime.date.today().strftime('%Y-%m-%d')}.mp3", "wb") as file:
         gTTS(text, lang='hu').write_to_fp(file)
 
     quit()
+
 
 with open("config.yaml", "r") as config:
     try:
@@ -16,13 +19,9 @@ with open("config.yaml", "r") as config:
         write_mp3("A konfigurációs fájl szintaxisa helytelen. ")
 
 
-
-from blocks import date, greeting, weather, joplin
-
 SAYSTRING = ""
 SAYSTRING += greeting.say(CONFIG)
 SAYSTRING += date.say(CONFIG)
 SAYSTRING += weather.say(CONFIG)
-SAYSTRING += joplin.say(CONFIG)
 
 write_mp3(SAYSTRING)
